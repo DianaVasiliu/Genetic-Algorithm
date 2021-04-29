d80f58c2a88b6d78d4e219a114528285057c1da6
dianavasiliu10 <diana.vasiliu10@gmail.com>
2021-04-29 20:47:18 +0300

    Added input data validation

--- src/readInput.py
+++ src/readInput.py
@@ -3,18 +3,43 @@ import re
 
 def readInput(inputFile):
     # population dimension
-    n = int(re.findall(r"[-+]?\d*\.\d+|\d+", inputFile.readline())[0])
+    n = float(re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())[0])
     # function's domain
-    domain = [int(x) for x in re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())]
+    domain = [float(x) for x in re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())]
     # function's coefficients
-    coeff = [int(x) for x in re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())]
+    coeff = [float(x) for x in re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())]
     # precision (the number of decimals used)
-    precision = int(re.findall(r"[-+]?\d*\.\d+|\d+", inputFile.readline())[0])
+    precision = float(re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())[0])
     # probability of crossover
-    pc = float(re.findall(r"[-+]?\d*\.\d+|\d+", inputFile.readline())[0])
+    pc = float(re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())[0])
     # probability of mutation
-    pm = float(re.findall(r"[-+]?\d*\.\d+|\d+", inputFile.readline())[0])
+    pm = float(re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())[0])
     # number of steps of the algorithm
-    noOfSteps = int(re.findall(r"[-+]?\d*\.\d+|\d+", inputFile.readline())[0])
+    noOfSteps = float(re.findall(r"[-+]?\d*\.\d+|[-+]?\d+", inputFile.readline())[0])
 
+    validData = checkInputData(n, domain, precision, pc, pm, noOfSteps)
+    if not validData:
+        print("Invalid input data")
+        exit(0)
+
+    n = int(n)
+    precision = int(precision)
+    noOfSteps = int(noOfSteps)
     return n, domain, coeff, precision, pc, pm, noOfSteps
+
+
+def checkInputData(n, domain, precision, pc, pm, noOfSteps):
+    if n <= 0 or not n.is_integer():
+        return False
+    if domain[0] > domain[1]:
+        return False
+    if precision <= 0 or not precision.is_integer():
+        return False
+    if pc < 0 or pc > 1:
+        return False
+    if pm < 0 or pm > 1:
+        return False
+    if noOfSteps <= 0 or not noOfSteps.is_integer():
+        return False
+
+    return True
